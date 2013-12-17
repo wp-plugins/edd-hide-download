d848059ab8d41bc56a69453edd32d3205c4e0e24
sumobi <sumobi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-17 23:09:04 +0000

    version 1.1.4
    git-svn-id: https://plugins.svn.wordpress.org/edd-hide-download/tags/1.1.4@824253 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ edd-hide-download.php
@@ -0,0 +1,195 @@
+<?php
+/*
+Plugin Name: Easy Digital Downloads - Hide Download
+Plugin URI: http://sumobi.com/shop/edd-hide-download/
+Description: Allows a download to be hidden as well as preventing direct access to the download
+Version: 1.1.4
+Author: Andrew Munro, Sumobi
+Author URI: http://sumobi.com/
+License: GPL-2.0+
+License URI: http://www.opensource.org/licenses/gpl-license.php
+*/
+
+
+if ( !class_exists( 'EDD_Hide_Download' ) ) {
+
+	class EDD_Hide_Download {
+
+		/**
+		 * Keep the hidden downloads in options
+		 *
+		 * @since  1.0.0
+		 * @var    array
+		 */
+		private $hidden_downloads;
+
+		function __construct() {
+			add_action( 'init', array( $this, 'textdomain' ) );
+			add_action( 'edd_meta_box_fields', array( $this, 'add_metabox' ), 100 );
+			add_action( 'edd_metabox_fields_save', array( $this, 'save_metabox' ) );
+			add_action( 'pre_get_posts',  array( $this, 'pre_get_posts' ), 9999 );
+			add_filter( 'edd_downloads_query', array( $this, 'shortcode_query' ) );
+
+			// find all hidden products on metabox render
+			add_action( 'edd_meta_box_fields', array( $this, 'query_hidden_downloads' ), 90 );
+
+			// redirect if product is set to be hidden
+			add_action( 'template_redirect', array( $this, 'redirect_hidden' ) );
+
+			// load the hidden downloads
+			$this->hidden_downloads = get_option( 'edd_hd_ids', array() );
+
+		}
+
+		/**
+		 * Internationalization
+		 *
+		 * @since 1.0
+		 */
+		function textdomain() {
+			load_plugin_textdomain( 'edd-hd', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+		}
+
+		/**
+		 * Add Metabox
+		 *
+		 * @since 1.0
+		*/
+		function add_metabox( $post_id ) {
+			$checked = (boolean) get_post_meta( $post_id, '_edd_hide_download', true );
+			$is_redirect = (boolean) get_post_meta( $post_id, '_edd_hide_redirect_download', true );
+		?>
+			<p><strong><?php apply_filters( 'edd_hide_download_header', printf( __( 'Hide %s', 'edd-hd' ), edd_get_label_singular() ) ); ?></strong></p>
+			<p>
+				<label for="edd_hide_download">
+					<input type="checkbox" name="_edd_hide_download" id="edd_hide_download" value="1" <?php checked( true, $checked ); ?> />
+					<?php apply_filters( 'edd_hide_download_label', printf( __( 'Hide this %s', 'edd-hd' ), strtolower( edd_get_label_singular() ) ) ); ?>
+				
+				</label>
+			</p>
+			<p>
+				<label for="edd_hide_redirect_download">
+					<input type="checkbox" name="_edd_hide_redirect_download" id="edd_hide_redirect_download" value="1" <?php checked( true, $is_redirect ); ?> />
+						<?php apply_filters( 'edd_hide_download_disable_access_label', printf( __( 'Disable direct access to this %s', 'edd-hd' ), strtolower( edd_get_label_singular() ) ) ); ?>
+				</label>
+			</p>
+
+		<?php
+		}
+
+
+		/**
+		 * Add to save function
+		 *
+		 * @since 1.0
+		*/
+		function save_metabox( $fields ) {
+			$fields[] = '_edd_hide_download';
+			$fields[] = '_edd_hide_redirect_download';
+
+			return $fields;
+		}
+
+
+		/**
+		 * Store the hidden products ids in the options table
+		 *  @since 1.1
+		 */
+		function query_hidden_downloads() {
+			
+			$args = array(
+				'post_type' => 'download',
+				'meta_key' => '_edd_hide_download',
+				'posts_per_page' => -1
+			);
+
+			$downloads = get_posts( $args );
+
+			$hidden_downloads = array();
+
+			if ( $downloads ) {
+				foreach ( $downloads as $download ) {
+					$hidden_downloads[] = $download->ID;
+				}
+			}
+			
+			update_option( 'edd_hd_ids', $hidden_downloads );
+
+		}
+		
+
+		/**
+		 * Get array hidden downloads
+		 *
+		 * @since 1.0
+		*/
+		function get_hidden_downloads() {			
+
+			return $this->hidden_downloads;
+		}
+
+		/**
+		 * Hook into shortcode query and modify
+		 *
+		 * @since 1.0
+		*/
+		function shortcode_query( $query ) {
+			$query['post__not_in'] = $this->get_hidden_downloads();
+
+			return $query;
+		}
+
+
+		/**
+		 * Alter the main loop to hide download using pre_get_posts
+		 * We're not using ! is_main_query because no matter what the query is on the page we want to hide them
+		 * @since 1.0
+		 */
+		function pre_get_posts( $query ) {
+
+			// bail if in the admin
+			if ( is_admin() )
+				return;
+
+			// hide downloads from all queries except singular pages, which will 404 without the conditional
+			// is_singular('download') doesn't work inside pre_get_posts
+			
+			if( ! $query->is_single )
+				$query->set( 'post__not_in', $this->get_hidden_downloads() );
+
+		}
+
+
+		/**
+		 * Redirect if product needs to be hidden
+		 *  @since 1.1
+		 */
+		function redirect_hidden() {
+			global $post;
+
+			 if ( ! in_array( $post->ID, $this->hidden_downloads ) )
+			 	return;	
+
+			 $is_redirect_active = (boolean) get_post_meta( $post->ID, '_edd_hide_redirect_download', true );
+
+			 if ( $is_redirect_active ) {
+
+				$redirect_url = site_url();
+
+				if ( isset( $_REQUEST['HTTP_REFERER'] ) ) {
+					$referer = esc_url( $_REQUEST['HTTP_REFERER '] );
+
+					if ( strpos( $referer, $redirect_url ) !== false )
+						$redirect_url = $referer;
+				}
+
+				wp_redirect( $redirect_url, 301 ); exit;
+				
+			}
+			
+		}
+
+	}
+
+}
+$EDD_Hide_Download = new EDD_Hide_Download();
---
+++ readme.txt
@@ -0,0 +1,96 @@
+=== EDD Hide Download ===
+Contributors: sumobi, alex.i
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=EFUPMPEZPGW7L
+Tags: easy digital downloads, digital downloads, e-downloads, edd, hide, e-commerce, ecommerce, hidden, sumobi
+Requires at least: 3.3
+Tested up to: 3.6.1
+Stable tag: 1.1.4
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Allows a download to be hidden as well as preventing direct access to the download
+
+== Description ==
+
+This plugin requires [Easy Digital Downloads](http://wordpress.org/extend/plugins/easy-digital-downloads/ "Easy Digital Downloads"). 
+
+It allows you to:
+
+1. Hide a download so it doesn't appear on the custom post type archive page, anywhere where the [downloads] shortcode is being used, or any custom query on a page template
+1. Prevent direct access to the download. The browser will redirect the user to the site's homepage.
+
+This plugin is extremely useful in the following situations:
+
+1. You've created a product landing page and inserted a buy now button to your product. Since the landing page contains all the required product information, you can hide the product on the rest of your site and even prevent direct access to it.
+1. You've added a product (eg support package) that shouldn't sit with your other products you have listed. In this case we can simply hide it from appearing with the other products and insert it where we'd like it to appear using the shortcode. 
+
+**Looking for the perfect Easy Digital Downloads theme?**
+
+[http://wordpress.org/themes/shop-front/](http://wordpress.org/themes/shop-front/ "Shop Front")
+
+Shop Front is a simple, responsive & easily extensible theme for the Easy Digital Downloads plugin. It also functions perfectly without the plugin as a standard WordPress blog. A free child theme for modifications can be downloaded from [http://sumobi.com/shop/shop-front-child-theme](http://sumobi.com/shop/shop-front-child-theme "Shop Front Child Theme") as well as other free and paid add-ons to enhance the functionality and styling.
+
+**Stay up to date**
+
+*Subscribe to updates* 
+[http://sumobi.com](http://sumobi.com "Sumobi")
+
+*Become a fan on Facebook* 
+[http://www.facebook.com/sumobicom](http://www.facebook.com/sumobicom "Facebook")
+
+*Follow me on Twitter* 
+[http://twitter.com/sumobi_](http://twitter.com/sumobi_ "Twitter")
+
+== Installation ==
+
+1. Unpack the entire contents of this plugin zip file into your `wp-content/plugins/` folder locally
+1. Upload to your site
+1. Navigate to `wp-admin/plugins.php` on your site (your WP Admin plugin page)
+1. Activate this plugin
+
+OR you can just install it with WordPress by going to Plugins >> Add New >> and type this plugin's name
+
+After activation, a new "Hide Download" section will appear at the bottom of Easy Digital Download's Download Configuration metabox
+
+== Screenshots ==
+
+1. The new options added to the bottom of Easy Digital Download's Download Configuration metabox
+
+
+== Upgrade Notice ==
+
+= 1.1.4 =
+Fixed compatibility issue between EDD Tickets and EDD Hide Download that was causing downloads to not be hidden
+
+= 1.1.3 =
+Fixed bug where some downloads were not showing
+
+== Changelog ==
+
+= 1.1.4 =
+* Fix: compatibility issue between EDD Tickets and EDD Hide Download that was causing downloads to not be hidden
+
+
+= 1.1.3 =
+* Fix: Fixed bug where some downloads were not showing
+
+= 1.1.2 =
+* Fix: Fixed downloads not showing when using page templates
+
+= 1.1.1 =
+* New: Now hides downloads in page templates using custom queries
+* Tweak: Modified text in configuration options
+* Tweak: Updated .pot file with modified text strings
+
+= 1.1 =
+* New: Option for disabling direct access to the download. User will be redirected to homepage. Props alex.i
+* Tweak: Updated .pot file with new localized text strings
+
+= 1.0.1 =
+* Tweak: Moved to bottom of download configuration metabox and added a title.
+* Tweak: Removed unused constants function
+* New: edd_hide_download_label filter for changing the checkbox label
+* New: edd_hide_download_header filter for changing the title text
+
+= 1.0 =
+* Initial release
