d59c03b36d46319eca8f903997cdad1b79c3710f
sumobi <sumobi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-11 08:10:49 +0000

    tagging 1.2.2
    git-svn-id: https://plugins.svn.wordpress.org/edd-hide-download/tags/1.2.2@891145 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- edd-hide-download.php
+++ edd-hide-download.php
@@ -3,7 +3,7 @@
 Plugin Name: Easy Digital Downloads - Hide Download
 Plugin URI: http://sumobi.com/shop/edd-hide-download/
 Description: Allows a download to be hidden as well as preventing direct access to the download
-Version: 1.2.1
+Version: 1.2.2
 Author: Andrew Munro, Sumobi
 Author URI: http://sumobi.com/
 License: GPL-2.0+
@@ -90,7 +90,7 @@ if ( ! class_exists( 'EDD_Hide_Download' ) ) {
 		 * @return void
 		 */
 		private function setup_globals() {
-			$this->version 		= '1.2.1';
+			$this->version 		= '1.2.2';
 			$this->title 		= 'EDD Hide Download';
 
 			// paths
@@ -316,14 +316,16 @@ if ( ! class_exists( 'EDD_Hide_Download' ) ) {
 		 */
 		function pre_get_posts( $query ) {
 
-			// bail if in the admin
-			if ( is_admin() )
+			$is_bbpress = function_exists( 'is_bbpress' ) ? is_bbpress() : false;
+
+			// bail if in the admin or on bbpress page
+			if ( is_admin() || $is_bbpress )
 				return;
 
 			// hide downloads from all queries except singular pages, which will 404 without the conditional
 			// is_singular('download') doesn't work inside pre_get_posts
 			
-			if ( ! ( $query->is_single || is_bbpress() ) ) {
+			if ( ! $query->is_single ) {
 				$excluded_ids = isset( $query->query_vars['post__not_in'] ) ? $query->query_vars['post__not_in'] : array();
 				// make sure we're merging with existing post__not_in so we do not override it
 				$query->set( 'post__not_in', array_merge( $excluded_ids, $this->get_hidden_downloads() ) );
